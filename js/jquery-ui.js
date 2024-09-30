8366223de5bc02747146e79629638f5f045f35e6
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-09-30 16:18:21 -0400

    replace ui call

--- js/jquery-ui.js
+++ js/jquery-ui.js
@@ -2,6 +2,12 @@ $( function() {
   $( ".ui:not(.no_controls)" ).draggable({ handle: ".ui_header" });
 
   document.querySelectorAll('.minimize').forEach((minimizeButton)=>{
+    console.log(minimizeButton)
     minimizeButton.addEventListener('click', toggleMinimize)
   })
 } );
+
+function toggleMinimize(e){
+	let ui = e.target.parentElement.parentElement;
+	ui.classList.toggle('minimized');
+}
