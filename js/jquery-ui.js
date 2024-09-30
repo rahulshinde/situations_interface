2287fb70a44ebb500a182f03836c61b2897482de
Rahul S. Shinde <rsubhashshinde@gmail.com>
2024-09-30 16:06:53 -0400

    update minimization

--- js/jquery-ui.js
+++ js/jquery-ui.js
@@ -5,8 +5,3 @@ $( function() {
     minimizeButton.addEventListener('click', toggleMinimize)
   })
 } );
-
-function toggleMinimize(e){
-  let ui = e.target.parentElement.parentElement;
-  ui.classList.toggle('minimized');
-}
